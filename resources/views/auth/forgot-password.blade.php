17d9610ac7cf59a20cf00fdc5b967dfb1fde32c0
seventejero <elletejero09@gmail.com>
2024-05-26 00:37:02 +0800

    Nice Admin successfully Integrated!

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,20 +4,20 @@
     </div>
 
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <x-auth-session-status class="mb-4" :status="session('status')"  />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" class="row g-3 needs-validation">
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="col-12">
+            <x-input-label for="email" :value="__('Email')" class="form-label"/>
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-error :messages="$errors->get('email')" class="invalid-feedback" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="btn btn-primary w-100">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
